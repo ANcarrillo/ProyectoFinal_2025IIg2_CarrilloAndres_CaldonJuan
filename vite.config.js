a8d3110852fa6e121e92a222ea63896b103cbc39
Andres Carrillo <67724771+ANcarrillo@users.noreply.github.com>
2025-11-09 22:12:29 -0500

    y

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ProyectoFinal_2025IIg2_CarrilloAndres_CaldonJuan/',  
   plugins: [react()],
-
-})
+  base: '/ProyectoFinal_2025IIg2_CarrilloAndres_CaldonJuan/',
+});
